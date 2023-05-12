465183c029bc0974beaaa337b3d0452460ead6e1
YoussifSelima1 <133367924+YoussifSelima1@users.noreply.github.com>
2023-05-12 22:55:35 +0300

    Add files via upload

---
+++ OS assignment 3.py
@@ -0,0 +1,326 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1649, 900)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 90, 81, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 70, 47, 13))
+        self.label.setObjectName("label")
+        self.processbutton = QtWidgets.QPushButton(self.centralwidget)
+        self.processbutton.setGeometry(QtCore.QRect(200, 130, 131, 23))
+        self.processbutton.setObjectName("processbutton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(180, 70, 47, 13))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(300, 70, 47, 13))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(430, 70, 47, 13))
+        self.label_4.setObjectName("label_4")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(170, 90, 81, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(290, 90, 81, 21))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(40, 230, 81, 21))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_5.setGeometry(QtCore.QRect(410, 90, 81, 21))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(50, 210, 61, 16))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(170, 210, 61, 16))
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(400, 210, 61, 16))
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(290, 210, 61, 16))
+        self.label_8.setObjectName("label_8")
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_6.setGeometry(QtCore.QRect(160, 230, 81, 21))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_7.setGeometry(QtCore.QRect(280, 230, 81, 21))
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.lineEdit_8 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_8.setGeometry(QtCore.QRect(390, 230, 81, 21))
+        self.lineEdit_8.setObjectName("lineEdit_8")
+        self.resourcebutton = QtWidgets.QPushButton(self.centralwidget)
+        self.resourcebutton.setGeometry(QtCore.QRect(190, 270, 151, 23))
+        self.resourcebutton.setObjectName("resourcebutton")
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(220, 40, 47, 13))
+        self.label_9.setObjectName("label_9")
+        self.label_10 = QtWidgets.QLabel(self.centralwidget)
+        self.label_10.setGeometry(QtCore.QRect(250, 180, 71, 20))
+        self.label_10.setObjectName("label_10")
+        self.label_11 = QtWidgets.QLabel(self.centralwidget)
+        self.label_11.setGeometry(QtCore.QRect(250, 330, 91, 20))
+        self.label_11.setObjectName("label_11")
+        self.lineEdit_9 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_9.setGeometry(QtCore.QRect(290, 380, 81, 21))
+        self.lineEdit_9.setObjectName("lineEdit_9")
+        self.lineEdit_10 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_10.setGeometry(QtCore.QRect(50, 380, 81, 21))
+        self.lineEdit_10.setObjectName("lineEdit_10")
+        self.label_12 = QtWidgets.QLabel(self.centralwidget)
+        self.label_12.setGeometry(QtCore.QRect(300, 360, 61, 16))
+        self.label_12.setObjectName("label_12")
+        self.lineEdit_11 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_11.setGeometry(QtCore.QRect(400, 380, 81, 21))
+        self.lineEdit_11.setObjectName("lineEdit_11")
+        self.label_13 = QtWidgets.QLabel(self.centralwidget)
+        self.label_13.setGeometry(QtCore.QRect(180, 360, 61, 16))
+        self.label_13.setObjectName("label_13")
+        self.label_14 = QtWidgets.QLabel(self.centralwidget)
+        self.label_14.setGeometry(QtCore.QRect(410, 360, 61, 16))
+        self.label_14.setObjectName("label_14")
+        self.lineEdit_12 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_12.setGeometry(QtCore.QRect(170, 380, 81, 21))
+        self.lineEdit_12.setObjectName("lineEdit_12")
+        self.label_15 = QtWidgets.QLabel(self.centralwidget)
+        self.label_15.setGeometry(QtCore.QRect(60, 360, 61, 16))
+        self.label_15.setObjectName("label_15")
+        self.allocationtable = QtWidgets.QTableWidget(self.centralwidget)
+        self.allocationtable.setGeometry(QtCore.QRect(710, 40, 411, 171))
+        self.allocationtable.setObjectName("allocationtable")
+        self.allocationtable.setColumnCount(4)
+        self.allocationtable.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.allocationtable.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.allocationtable.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.allocationtable.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.allocationtable.setHorizontalHeaderItem(3, item)
+        self.calculatebutton = QtWidgets.QPushButton(self.centralwidget)
+        self.calculatebutton.setGeometry(QtCore.QRect(190, 420, 151, 23))
+        self.calculatebutton.setObjectName("calculatebutton")
+        self.availablematrix = QtWidgets.QTableWidget(self.centralwidget)
+        self.availablematrix.setGeometry(QtCore.QRect(600, 500, 311, 161))
+        self.availablematrix.setObjectName("availablematrix")
+        self.availablematrix.setColumnCount(3)
+        self.availablematrix.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.availablematrix.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.availablematrix.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.availablematrix.setHorizontalHeaderItem(2, item)
+        self.maxallocation = QtWidgets.QTableWidget(self.centralwidget)
+        self.maxallocation.setGeometry(QtCore.QRect(600, 290, 311, 171))
+        self.maxallocation.setObjectName("maxallocation")
+        self.maxallocation.setColumnCount(3)
+        self.maxallocation.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.maxallocation.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.maxallocation.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.maxallocation.setHorizontalHeaderItem(2, item)
+        self.needmatrix = QtWidgets.QTableWidget(self.centralwidget)
+        self.needmatrix.setGeometry(QtCore.QRect(970, 500, 311, 161))
+        self.needmatrix.setObjectName("needmatrix")
+        self.needmatrix.setColumnCount(3)
+        self.needmatrix.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.needmatrix.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.needmatrix.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.needmatrix.setHorizontalHeaderItem(2, item)
+        self.label_16 = QtWidgets.QLabel(self.centralwidget)
+        self.label_16.setGeometry(QtCore.QRect(720, 470, 111, 20))
+        self.label_16.setObjectName("label_16")
+        self.label_17 = QtWidgets.QLabel(self.centralwidget)
+        self.label_17.setGeometry(QtCore.QRect(690, 260, 111, 20))
+        self.label_17.setObjectName("label_17")
+        self.label_18 = QtWidgets.QLabel(self.centralwidget)
+        self.label_18.setGeometry(QtCore.QRect(830, 10, 151, 16))
+        self.label_18.setObjectName("label_18")
+        self.label_19 = QtWidgets.QLabel(self.centralwidget)
+        self.label_19.setGeometry(QtCore.QRect(1080, 470, 101, 16))
+        self.label_19.setObjectName("label_19")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1649, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Banker's Algorithm"))
+        self.label.setText(_translate("MainWindow", "process "))
+        self.processbutton.setText(_translate("MainWindow", "Add process"))
+        self.label_2.setText(_translate("MainWindow", "Resource"))
+        self.label_3.setText(_translate("MainWindow", "Resource"))
+        self.label_4.setText(_translate("MainWindow", "Resource"))
+        self.label_5.setText(_translate("MainWindow", "process"))
+        self.label_6.setText(_translate("MainWindow", "Resource"))
+        self.label_7.setText(_translate("MainWindow", "Resource"))
+        self.label_8.setText(_translate("MainWindow", "Resource"))
+        self.resourcebutton.setText(_translate("MainWindow", "Add Resources"))
+        self.label_9.setText(_translate("MainWindow", "Allocation"))
+        self.label_10.setText(_translate("MainWindow", "Max Matrix"))
+        self.label_11.setText(_translate("MainWindow", "Available"))
+        self.label_12.setText(_translate("MainWindow", "Resource "))
+        self.label_13.setText(_translate("MainWindow", "Resource "))
+        self.label_14.setText(_translate("MainWindow", "Resource "))
+        self.label_15.setText(_translate("MainWindow", "process"))
+        item = self.allocationtable.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Process"))
+        item = self.allocationtable.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "A"))
+        item = self.allocationtable.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "B"))
+        item = self.allocationtable.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "C"))
+        self.calculatebutton.setText(_translate("MainWindow", "Calculate"))
+        item = self.availablematrix.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "A"))
+        item = self.availablematrix.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "B"))
+        item = self.availablematrix.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "C"))
+        item = self.maxallocation.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "A"))
+        item = self.maxallocation.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "B"))
+        item = self.maxallocation.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "C"))
+        item = self.needmatrix.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "A"))
+        item = self.needmatrix.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "B"))
+        item = self.needmatrix.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "C"))
+        self.label_16.setText(_translate("MainWindow", "Available Matrix"))
+        self.label_17.setText(_translate("MainWindow", "Max Allocation"))
+        self.label_18.setText(_translate("MainWindow", "Allocation table"))
+        self.label_19.setText(_translate("MainWindow", "Need Matrix"))
+        
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.processbutton.clicked.connect(self.add_process)
+        self.resourcebutton.clicked.connect(self.add_resource)
+        self.calculatebutton.clicked.connect(self.calculate)
+
+        self.processes = []
+        self.resources = []
+        self.allocation = []
+        self.max_allocation = []
+
+    def add_process(self):
+        process = []
+        process.append(self.lineEdit.text())
+        process.append(self.lineEdit_2.text())
+        process.append(self.lineEdit_3.text())
+        process.append(self.lineEdit_5.text())
+        process.append(self.lineEdit_5.text())
+        self.processes.append(process)
+        self.update_table(self.allocationtable, self.processes)
+
+    def add_resource(self):
+        resource = [self.lineEdit_4.text(), self.lineEdit_6.text(), self.lineEdit_7.text(), self.lineEdit_8.text()]
+        self.resources.append(resource)
+        self.update_table(self.maxallocation, self.resources)
+
+    def calculate(self):
+        self.get_data()
+
+        # Calculate the need matrix
+        need_matrix = []
+        for i in range(len(self.processes)):
+            allocation_row = self.allocation[i][1:]
+            max_allocation_row = self.max_allocation[i][1:]
+            need_row = list(map(lambda x, y: int(y) - int(x), allocation_row, max_allocation_row))
+            need_matrix.append([self.allocation[i][0]] + need_row)
+
+        # Calculate the available resources vector
+        available_resources = self.calculate_available_resources()
+
+        # Check if the system is in a safe state
+        safe_sequence = self.check_safe_state(need_matrix, available_resources)
+
+        # Show the result
+        if safe_sequence:
+            QtWidgets.QMessageBox.information(self, 'Safe state', f'Found a safe sequence: {", ".join(safe_sequence)}')
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Unsafe state', 'The system is in an unsafe state')
+
+    def get_data(self):
+        self.allocation = self.get_table_data(self.allocationtable)
+        self.max_allocation = self.get_table_data(self.maxallocation)
+        self.resources = self.get_table_data(self.availablematrix)
+
+    def get_table_data(self, table):
+        data = []
+        for row in range(table.rowCount()):
+            row_data = []
+            for column in range(table.columnCount()):
+                item = table.item(row, column)
+                row_data.append(item.text())
+            data.append(row_data)
+        return data
+
+    def update_table(self, table, data):
+        table.setRowCount(len(data))
+        for i, row_data in enumerate(data):
+            for j, cell_data in enumerate(row_data):
+                item = QtWidgets.QTableWidgetItem(cell_data)
+                table.setItem(i, j, item)
+
+    def calculate_available_resources(self):
+        available_resources = []
+        for i in range(len(self.resources[0])):
+            total = sum(int(resource[i]) for resource in self.resources)
+            allocated = sum(int(process[i + 1]) for process in self.allocation)
+            available = total - allocated
+            available_resources.append(str(available))
+        return available_resources
+
+    def check_safe_state(self, need_matrix, available_resources):
+        processes_done = [False] * len(self.processes)
+        safe_sequence = []
+
+        while not all(processes_done):
+            found = False
+            for i, process in enumerate(self.processes):
+                if not processes_done[i] and all(int(need) <= int(available_resources[j]) for j, need in enumerate(need_matrix[i][1:])):
+                    processes_done[i] = True
+                    available_resources = list(map(lambda x,y: str(int(x)+int(y)), available_resources, self.allocation[i][1:]))
+                    safe_sequence.append(process[0])
+                    found = True
+            if not found:
+                break
+
+        return safe_sequence if len(safe_sequence) == len(self.processes) else None
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MainWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
